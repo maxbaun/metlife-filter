e78fe8445e3761f6d5862fc7556a860e6c613b2b
Max Baun <max@d3applications.com>
2017-04-13 17:31:57 -0400

    finished styling

--- src/templates/shortcode-filter.php
+++ src/templates/shortcode-filter.php
@@ -6,32 +6,44 @@ use D3\MLF\Dropdown;
 
 <div class="mlf-filter">
 	<form id="mlf_filter_form_1" class="mlf-filter-form" action="index.html" method="post">
-		<span class="mlf-filter-title">Filter By</span>
-		<?php
-			$agentDropdown = new Dropdown(array(
-				'title' => 'Agent Channel(s)',
-				'taxonomy' => 'agent_channel'
-			));
+		<div class="mlf-filter-row">
+			<div class="mlf-filter-col-1 mlf-filter-border-right mlf-filter-border-dark mlf-hidden-tablet">
+				<span class="mlf-filter-title mlf-hidden-mobile">Filter By</span>
+			</div>
+			<div class="mlf-filter-col-2 mlf-filter-border-left mlf-filter-border-light mlf-float-right-mobile mlf-no-borders-tablet">
+				<?php
+					$agentDropdown = new Dropdown(array(
+						'title' => 'Agent Channel(s)',
+						'taxonomy' => 'agent_channel'
+					));
 
-			$agentDropdown->render();
-		?>
-		<?php
-			$stateDropdown = new Dropdown(array(
-				'title' => 'Appointed State(s)',
-				'taxonomy' => 'appointed_state'
-			));
+					$agentDropdown->render();
+				?>
+			</div>
+			<div class="mlf-filter-col-2 mlf-filter-border-left mlf-filter-border-light mlf-float-right-mobile mlf-no-borders-mobile">
+				<?php
+					$stateDropdown = new Dropdown(array(
+						'title' => 'Appointed State(s)',
+						'taxonomy' => 'appointed_state'
+					));
 
-			$stateDropdown->render();
-		?>
-		<div class="mlf-filter-form-submit">
-			<input
-				type="submit"
-				class="mlf-filter-button"
-				name="mlf_filter_form_submit"
-				id="mlf_filter_form_submit"
-				value="Apply"
-				>
+					$stateDropdown->render();
+				?>
+			</div>
+			<div class="mlf-filter-col-1 mlf-float-right-mobile">
+				<div class="mlf-filter-form-submit">
+					<input
+						type="submit"
+						class="mlf-filter-button"
+						name="mlf_filter_form_submit"
+						id="mlf_filter_form_submit"
+						value="Apply"
+						>
+				</div>
+			</div>
+			<div class="mlf-filter-col-1 mlf-hidden-mobile">
+				<a class="mlf-filter-link" href="#">Advanced Search</a>
+			</div>
 		</div>
-		<a class="mlf-filter-link" href="#">Advanced Search</a>
 	</form>
 </div>
