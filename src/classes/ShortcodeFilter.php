bdeb539536054af18239e51c6966bddc2c30b6a0
Max Baun <max@d3applications.com>
2017-04-13 19:37:34 -0400

    adding visible on pages helper

--- src/classes/ShortcodeFilter.php
+++ src/classes/ShortcodeFilter.php
@@ -13,14 +13,35 @@ class ShortcodeFilter
 
 	public static function render($args, $content = "")
 	{
+
 		$advancedSearchText = AdminSettings::getOption('metlife_filter_advanced_search_text');
 		$advancedSearchPageId = AdminSettings::getOption('metlife_filter_advanced_search_page');
 		$advancedSearchPage = get_permalink($advancedSearchPageId);
 
-		ob_start();
-		include_once(plugin_dir_path(dirname(__FILE__)) . 'templates/shortcode-filter.php');
-		$output = ob_get_clean();
-		ob_end_flush();
-		return force_balance_tags($output);
+		if (self::isVisibleOnPage()) {
+			ob_start();
+			include_once(plugin_dir_path(dirname(__FILE__)) . 'templates/shortcode-filter.php');
+			$output = ob_get_clean();
+			ob_end_flush();
+			return force_balance_tags($output);
+		} else {
+			return '';
+		}
+	}
+
+	public static function isVisibleOnPage()
+	{
+		global $post;
+		$showOnPages = AdminSettings::getOption('metlife_filter_show_on_pages');
+
+		if (!is_page()) {
+			return true;
+		}
+
+		if (!in_array($post->ID, $showOnPages)) {
+			return false;
+		}
+
+		return true;
 	}
 }
